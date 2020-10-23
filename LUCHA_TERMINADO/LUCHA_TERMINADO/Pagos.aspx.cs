95f7c848ba37ac4dd90e0dcb50d6da7f204d8089
carlosneos503 <63827845+carlosneos503@users.noreply.github.com>
2020-10-23 16:21:07 -0600

    CAMBIO2

---
+++ LUCHA_TERMINADO/LUCHA_TERMINADO/Pagos.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Pagos : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.Params["DUI"] != null)
+        {
+            GUARDO.Text = Request.Params["DUI"];
+            Label1.Text = Request.Params["NAME"];
+        }
+        else
+        {
+            Response.Redirect("index.aspx");
+        }
+    }
+
+    protected void Siguiente_Click(object sender, EventArgs e)
+    {
+        String Pago = DropDownList1.SelectedValue;
+        Response.Redirect("Factura.aspx?DUI=" + GUARDO.Text + "&USER=" + Label1.Text+"&PAGO="+Pago);
+
+
+
+    }
+}
---
+++ RESTAURANTE_LUCHA/RESTAURANTE_LUCHA/registrar.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class registrar : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        String Email;
+        String Password;
+        String US;
+        int Dui;
+        US = USER.Text;
+        Dui =Int32.Parse(DUI.Text);
+        Password=Encriptar.Encripta(CONTRA.Text);
+        Email = EMAIL.Text;
+        try
+        {
+            USUARIOBLL.Ingresar(Dui, US, Password, Email);
+            Response.Redirect("~/index.aspx");
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+}
